62dedacb4a93a9dcd398082f57a13602cca82250
hemmonash <hem.malhotra@monash.edu>
2019-03-16 15:36:49 +1100

    FEATURE: Vuforia and base UI setup with Firebase compelte

---
+++ AlphaAR-MobileApp/Assets/MainUIHandler.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using Firebase;
+using Firebase.Auth;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+using Firebase.Storage;
+using System.Threading.Tasks;
+
+
+public class MainUIHandler : MonoBehaviour
+{
+    public InputField sessionIDInput;
+    public Button startSessionButton;
+    public GameObject successImage;
+    public Boolean redirect = false;
+    public Boolean result = false;
+    public static int reuqiredNumberOfPrefabs = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // this is for the main firebase init
+        string readConfig = ReadString("Assets/Resources/config.txt");
+        string[] splitArray = readConfig.Split(char.Parse(";"));
+        Debug.Log("Array of config: " + splitArray[0]);
+
+        Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => {
+            var dependencyStatus = task.Result;
+            if (dependencyStatus == Firebase.DependencyStatus.Available)
+            {
+                // Create and hold a reference to your FirebaseApp,
+                // where app is a Firebase.FirebaseApp property of your application class.
+                FirebaseApp app = Firebase.FirebaseApp.DefaultInstance;
+
+                // Set a flag here to indicate whether Firebase is ready to use by your app.
+                Debug.Log("Firebase setup successfuly!");
+            }
+            else
+            {
+                UnityEngine.Debug.LogError(System.String.Format(
+                  "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
+                // Firebase Unity SDK is not safe to use here.
+            }
+        });
+
+        // add button listner fo the button start 
+        startSessionButton.onClick.AddListener(StartExperience);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (result == true)
+        {
+            // success, destroy button and show success image
+            successImage.SetActive(true);
+            startSessionButton.GetComponent<Button>().interactable = false;
+            startSessionButton.GetComponent<Button>().enabled = false;
+            StartCoroutine(WaitForRedirect());
+        }
+
+        Debug.Log("redirect: " + redirect.ToString());
+        if (redirect == true)
+        {
+            // move to ARScene
+            Debug.Log("Successful Auth, switching to AR scene");
+            SceneManager.LoadScene("ARScene");
+        }
+    }
+
+    // firebase auth function
+    public void FirebaseSessionAuthFunc()
+    {
+        // for return
+
+        String queryString = System.DateTime.Now.ToString("yyyy-MM-dd-HH");
+        //Debug.Log(queryString);
+        FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://alpha-ar.firebaseio.com/");
+        // Get the root reference location of the database.
+        DatabaseReference reference = FirebaseDatabase.DefaultInstance.RootReference;
+        FirebaseDatabase.DefaultInstance.GetReference(queryString).GetValueAsync().ContinueWith(task =>
+        {
+            if (task.IsFaulted)
+            {
+                // failed authentication
+
+                // Handle the error...
+                Debug.Log("Could not find collection");
+            }
+            else if (task.IsCompleted)
+            {
+
+                // authentication successful
+                DataSnapshot snapshot = task.Result;
+                // get the session id and compare the entered ID
+
+                // get the session code from firebase db and comapre to entered
+                String sessionIDEntered = sessionIDInput.text;
+                Debug.Log("snapshot value: " + snapshot.Value.ToString());
+                if (snapshot.Value.ToString() == sessionIDEntered.ToString())
+                {
+                    // Set true and then transition
+                    // redirect now
+                    // also get the number of prefabs required
+                    reuqiredNumberOfPrefabs = 8;
+                    string readConfig = ReadString("Assets/config.text");
+                    string[] splitArray = readConfig.Split(char.Parse(";"));
+                    result = true;
+                    Debug.Log("Array of config: " + splitArray);
+                }
+            }
+        });
+    }
+
+    // method for starting the experience
+    public void StartExperience()
+    {
+        String sessionID = sessionIDInput.text;
+        //Debug.Log("sessionID: " + sessionID.ToString());
+        // upon click run firebase auth
+        //Boolean result = FirebaseSessionAuthFunc();
+        FirebaseSessionAuthFunc();
+        Debug.Log("[SE] result: " + result.ToString());
+        //Debug.Log("[SE] result: " + result.ToString());
+        //successImage.SetActive(true);
+
+    }
+
+    static string ReadString(string path)
+    {
+        //string path = "Assets/Resources/test.txt";
+
+        //Read the text from directly from the test.txt file
+        StreamReader reader = new StreamReader(path);
+        string readConfig = reader.ReadLine().ToString();
+        reader.Close();
+
+        return readConfig;
+    }
+
+
+    public IEnumerator WaitForRedirect()
+    {
+        yield return new WaitForSecondsRealtime(2);
+        redirect = true;
+    }
+}
---
+++ AlphaAR-MobileApp/Assets/UserInterfaceButtons.cs
@@ -0,0 +1,300 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Threading.Tasks;
+using System.IO;
+
+public class UserInterfaceButtons : MonoBehaviour
+{
+	public float scalingSpeed = 0.1f;
+	public float rotationSpeed = 80.0f;
+	public float translationSpeed = 15.0f;
+    //	public GameObject Model;
+	bool repeatScaleUp = false;
+	bool repeatScaleDown = false;
+	bool repeatRotateLeft = false;
+	bool repeatRotateRight = false;
+	bool repeatPositionUp = false;
+	bool repeatPositionDown = false;
+	bool repeatPositionLeft = false;
+	bool repeatPositionRight = false;
+
+    // for model anims
+    public Canvas canvas;
+    public GameObject Model;
+	public Animator cellAnim;
+    // 8 placepholder button prefabs
+    public GameObject buttonPrefab_1;
+    public GameObject buttonPrefab_2;
+    public GameObject buttonPrefab_3;
+    public GameObject buttonPrefab_4;
+    public GameObject buttonPrefab_5;
+    public GameObject buttonPrefab_6;
+    public GameObject buttonPrefab_7;
+    public GameObject buttonPrefab_8;
+    bool animRan = false;
+    public float[] dynamicButtonPrefabs;
+
+    void Start()
+    {
+        // set display off to start with
+        buttonPrefab_1.SetActive(false);
+        buttonPrefab_2.SetActive(false);
+        buttonPrefab_3.SetActive(false);
+        buttonPrefab_4.SetActive(false);
+        buttonPrefab_5.SetActive(false);
+        buttonPrefab_6.SetActive(false);
+        buttonPrefab_7.SetActive(false);
+        buttonPrefab_8.SetActive(false);
+
+        // get the number of required prefabs
+        int requiredNuberOfPrefabs = MainUIHandler.reuqiredNumberOfPrefabs;
+
+        //Text temp = buttonPrefab_1.GetComponentInChildren<Text>();
+        //temp.text = "MITOCHONDRIA";
+
+        ReadConfigFile(requiredNuberOfPrefabs);
+    }
+
+    void Update ()
+    	{
+            
+    		if (repeatScaleUp) {
+    			ScaleUpButton ();
+    		}
+
+    		if (repeatScaleDown) {
+    			ScaleDownButton ();
+    		}
+
+    		if (repeatRotateRight) {
+    			RotationRightButton();
+    		}
+
+    		if (repeatRotateLeft) {
+    			RotationLeftButton();
+    		}
+
+    		if (repeatPositionUp) {
+    			PositionUpButton();
+    		}
+
+    		if (repeatPositionDown) {
+    			PositionDownButton();
+    		}
+
+    		if (repeatPositionLeft) {
+    			PositionLeftButton();
+    		}
+
+    		if (repeatPositionRight) {
+    			PositionRightButton();
+    		}
+        }
+
+    public void ShowMenu() 
+    {
+        Debug.Log("ShowMenu"); 
+    }
+    public void CloseAppButton ()
+	{
+		Application.Quit ();
+	}
+
+	public void RotationRightButton ()
+	{
+		// transform.Rotate (0, -rotationSpeed * Time.deltaTime, 0);
+		Model.transform.Rotate (0, -rotationSpeed * Time.deltaTime, 0);
+	}
+
+	public void RotationLeftButton ()
+	{
+        // transform.Rotate (0, rotationSpeed * Time.deltaTime, 0);
+        Model.transform.Rotate (0, rotationSpeed * Time.deltaTime, 0);
+	}
+
+	public void RotationRightButtonRepeat ()
+	{
+		// transform.Rotate (0, -rotationSpeed * Time.deltaTime, 0);
+		repeatRotateRight=true;
+	}
+	
+	public void RotationLeftButtonRepeat ()
+	{
+		// transform.Rotate (0, rotationSpeed * Time.deltaTime, 0);
+		repeatRotateLeft=true;
+	}
+
+	public void ScaleUpButton ()
+	{
+        // transform.localScale += new Vector3(scalingSpeed, scalingSpeed, scalingSpeed);
+        Model.transform.localScale += new Vector3 (scalingSpeed, scalingSpeed, scalingSpeed);
+    }
+
+	public void ScaleUpButtonRepeat ()
+	{
+		repeatScaleUp = true;
+		Debug.Log ("Up");
+	}
+	
+    public void ScaleDownButtonRepeat ()
+	{
+		repeatScaleDown = true;
+		Debug.Log ("Down");
+	}
+	
+    public void PositionDownButtonRepeat ()
+	{
+		repeatPositionDown = true;
+	}
+	
+    public void PositionUpButtonRepeat ()
+	{
+		repeatPositionUp = true;
+	}
+	
+    public void PositionLeftButtonRepeat ()
+	{
+		repeatPositionLeft = true;
+	}
+	
+    public void PositionRightButtonRepeat ()
+	{
+		repeatPositionRight = true;
+	}
+	
+	public void ScaleUpButtonOff ()
+	{
+		repeatScaleUp = false;
+		Debug.Log ("Off");
+	}
+	
+    public void ScaleDownButtonOff ()
+	{
+		repeatScaleDown = false;
+		Debug.Log ("Off");
+	}
+
+	public void RotateLeftButtonOff ()
+	{
+		repeatRotateLeft = false;
+		Debug.Log ("Off");
+	}
+
+	public void RotateRightButtonOff ()
+	{
+		repeatRotateRight = false;
+		Debug.Log ("Off");
+	}
+	
+    public void PositionRightButtonOff ()
+	{
+		repeatPositionRight = false;
+		Debug.Log ("Off");
+	}
+	
+    public void PositionLeftButtonOff ()
+	{
+		repeatPositionLeft = false;
+		Debug.Log ("Off");
+	}
+	
+    public void PositionUpButtonOff ()
+	{
+		repeatPositionUp = false;
+		Debug.Log ("Off");
+	}
+	
+    public void PositionDownButtonOff ()
+	{
+		repeatPositionDown = false;
+		Debug.Log ("Off");
+	}
+	
+	public void ScaleDownButton ()
+	{
+        // transform.localScale += new Vector3(-scalingSpeed, -scalingSpeed, -scalingSpeed);
+        Model.transform.localScale += new Vector3 (-scalingSpeed, -scalingSpeed, -scalingSpeed);
+	}
+
+	public void PositionUpButton ()
+	{
+        Model.transform.Translate (0, 0, -translationSpeed * Time.deltaTime);
+	}
+
+	public void PositionDownButton ()
+	{
+
+        Model.transform.Translate (0, 0, translationSpeed * Time.deltaTime);
+	}
+
+	public void PositionRightButton ()
+	{
+        Model.transform.Translate (-translationSpeed * Time.deltaTime, 0, 0);
+	}
+
+	public void PositionLeftButton ()
+	{
+        Model.transform.Translate (translationSpeed * Time.deltaTime, 0, 0);  // backward
+	}
+
+	public void ChangeScene (string a)
+	{
+		Application.LoadLevel (a);
+	}
+
+	public void AnyButton ()
+	{
+		Debug.Log ("Any");
+	}
+
+	public void PlayAnim (string animName)
+	{
+        Debug.Log("Aimation: " + animName.ToString());
+        if (animRan == false)
+        {
+            // for the initial run
+            cellAnim = Model.GetComponent<Animator>();
+            cellAnim.Play(animName);
+            animRan = true;
+        }
+        else if (animName == "explode")
+        {
+            // for the explode animation second run
+            cellAnim = Model.GetComponent<Animator>();
+            cellAnim.Play(animName.ToString() + "_reverse");
+            // reset the anim
+            animRan = false;
+        } else 
+        {
+            // for the other animations second run
+            cellAnim = Model.GetComponent<Animator>();
+            cellAnim.Play("Idle");
+            // reset the anim
+            animRan = false;
+        }
+    }
+
+    public void ReadConfigFile(int numberOfPrefabsRequired)
+    {
+        // instantiate the model animations using the config file
+        //Instantiate(buttonPrefab, );
+        //Debug.Log(buttonPrefab);
+        //buttonPrefab.transform.localPosition = new Vector2(-95,121);
+        //GameObject textInstance = Instantiate(buttonPrefab, transform.position, transform.rotation) as GameObject;
+        //textInstance.transform.SetParent(canvas.transform, false);
+        //Button buttonPrefabButtonComponent = buttonPrefab.GetComponent<Button>();
+        //buttonPrefabButtonComponent.onClick.AddListener(() => MyButtonDelegate("Nucleus"));
+
+        // based on how many prefabs are need for the ui buttons, enable the correct number
+        Debug.Log("Required Number of prefabs:" + numberOfPrefabsRequired.ToString());
+
+    }
+
+    public void MyButtonDelegate(string animName)
+    {
+        // play anim
+        Debug.Log("RENDEREDBUTTON: Clicked");
+        PlayAnim(animName);
+    }
+}   
