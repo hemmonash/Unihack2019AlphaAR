f524933e55a7967838f7e88e9c812013352bba7b
Nicola Halim <staticddq@gmail.com>
2019-03-16 20:53:09 +1100

    added basic asset bundle options

---
+++ AlphaAR-MobileApp/Assets/AssetBundles/BundleBuilder.cs
@@ -0,0 +1,10 @@
+﻿using UnityEditor;
+
+public class BundleBuilder : Editor {
+
+    [MenuItem("Assets/Build AssetBundles")]
+    static void BuildAllAssetBundles()
+    {
+        BuildPipeline.BuildAssetBundles(@"C:\Users\VoidDDQ\Desktop\AB", BuildAssetBundleOptions.ChunkBasedCompression, BuildTarget.StandaloneWindows);
+    }
+}
