a0328d932647d5150ac92cb962bdaf49fc8e2b3f
Unknown <fdarma@student.unimelb.edu.au>
2019-03-16 15:41:32 +1100

    added axios, prop-types, and react-router modules

---
+++ AlphaAR-WebApp/app/components/App.js
@@ -0,0 +1,27 @@
+var React = require('react');
+var ReactRouter =  require('react-router-dom');
+var Router = ReactRouter.BrowserRouter;
+var Route = ReactRouter.Route;
+var Home = require('./Home');
+var Switch = ReactRouter.Switch;
+
+
+class App extends React.Component{
+    render(){
+        return(
+            <Router>
+                <div className='container'>
+                    <Switch>
+                        <Route exact path='/' component={Home}/>
+                        <Route render={function(){
+                            return <p>Not Found :(</p>
+                        }}/>
+                    </Switch>
+                </div>
+            </Router>
+
+        )
+    }
+}
+
+module.exports = App;
